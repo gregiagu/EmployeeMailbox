4f10e2e101d96bfe926052e860c53dabccf05531
gregiagu <69273611+gregiagu@users.noreply.github.com>
2025-06-04 09:19:49 -0300

    Skeleton Project

---
+++ Specification/src/main/java/io/github/gregiagu/employeemailbox/App.java
@@ -0,0 +1,10 @@
+package io.github.gregiagu.employeemailbox;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
